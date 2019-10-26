fc3de8d56994172832e00d6dd666fadff3da72a4
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-26 13:05:03 -0400

    main page will display name and description

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,43 +1,57 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.models.Cheese;
+import org.launchcode.models.CheeseData;
+import org.launchcode.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import javax.validation.Valid;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<HashMap<String, String>> cheesesMapList = new ArrayList<HashMap<String, String>>();
-    static HashMap<String, String> cheesesMap = new HashMap<String, String>();
+//    static ArrayList<HashMap<String, String>> cheesesMapList = new ArrayList<HashMap<String, String>>();
+//    static HashMap<String, String> cheesesMap = new HashMap<String, String>();
 
     //  Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheesesMap", cheesesMapList);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
+
+//        for (Map.Entry<String, String> cheese : cheesesMap.entrySet()) {
+//            System.out.println(cheese.getKey() + ": " + cheese.getValue());
+//        }
+
+//        model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
 
     @RequestMapping(value = "add", method= RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+//        model.addAttribute("cheeseTypes", CheeseType.values());
+
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method= RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
-        cheesesMap.put(cheeseName, cheeseDescription);
-        cheesesMapList.add(cheesesMap);
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese, Errors errors, Model model) {
+//        @RequestParam String name, @RequestParam String description)
 
-
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
+        }
+        CheeseData.add(newCheese);
         return "redirect:";
     }
 }
